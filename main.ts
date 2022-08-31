f0b11c3b169bedd2d0ef456ae189ca6f81ddba58
jocpin1406 <112516790+jocpin1406@users.noreply.github.com>
2022-08-31 15:55:18 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+let Dice = 0
+input.onGesture(Gesture.Shake, function () {
+    Dice = randint(1, 6)
+    if (Dice == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Dice == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . . . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (Dice == 0) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+    } else if (Dice == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (Dice == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
